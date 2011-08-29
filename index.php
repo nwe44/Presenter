5611c84423f084c51b98895b5a511e6f5c493ce3
Nicholas Evans <nick@nick-evans.com>
2011-08-29 15:49:55 -0400

    Removed GA script

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ function getDirectory( $path = '.', $level = 0, $structure_array = array ()){
 				// parse the directory
 				$structure_array = getDirectory( "$path/$file", ($level+1), $structure_array); 
 
-			} else { 
+			} else {
 
 				if (function_exists('finfo_open')) {
 					// corral our file info
@@ -151,21 +151,6 @@ echo "<script> var presentations = " . $directories . ";</script>";
 		<div class="ir throbber"></div>
 	</script> 
 
-  <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
-       mathiasbynens.be/notes/async-analytics-snippet -->
-  <script>
-    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview'],['_trackPageLoadTime']];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
-  </script>
-
-  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
-       chromium.org/developers/how-tos/chrome-frame-getting-started -->
-  <!--[if lt IE 7 ]>
-    <script defer src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-    <script defer>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-  <![endif]-->
 
 </body>
 </html>
