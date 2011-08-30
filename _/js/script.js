557b6bab71dc109e6144d5b1d3c188e3a4462b9d
Nicholas Evans <nick@nick-evans.com>
2011-08-30 00:25:14 -0400

    Better separation of logic from DOM interaction.
    We should be better able to start writing some tests now.
    This resolves #6
    and, incidentally #5 too.

--- _/js/script.js
+++ _/js/script.js
@@ -4,10 +4,10 @@
 
 var presenter = {
 
+	status : {},
+
 	init : function () {
-		var liveContent = "",
-			that = this,
-			$nav = that.generateNav();
+		var $nav = this.generateNav();
 
 		$('header').append($nav);
 
@@ -15,83 +15,27 @@ var presenter = {
 			e.preventDefault();
 			var href = $(this).attr( "href" );
 			// Push this URL "state" onto the history hash.
-
+		
 			$.bbq.pushState({ p: href.substr(1),  s: "" });
-
-			// Prevent the default click behavior.
+		
 			return false;
 		});
-
+		
 		$('.nav-toggle').click(function(e){
 			e.preventDefault();
-			// Push this URL "state" onto the history hash.
+			var state =  $.bbq.getState();
 
-			$('header').toggleClass('minimized');
-			$('body').toggleClass('header-visible');
-			if ($('body').hasClass('header-visible')) {
-				$('.note-minimized').removeClass('note-minimized');
+			// toggle the menu state
+			// if the menu state is undefined
+			// set to true to open it
+			if (typeof(state.m) == "undefined") {
+				console.log('was undefined');
+				state.m = true;
 			} else {
-				$('.note').addClass('note-minimized');
+				state.m = (state.m == "true") ? false : true;
 			}
-			
+			$.bbq.pushState(state);
 			return false;
-		});
-
-		// Bind a callback that executes when document.location.hash changes.
-		$(window).bind( "hashchange", function(e) {
-
-			var presentationId = $.bbq.getState( "p" ),
-				slideNo = $.bbq.getState( "s" ),
-				presentation = presentations[presentationId];
-
-			$(".main-nav a").removeClass('nav-item-link-active');
-			$('#' + presentationId).addClass('nav-item-link-active');
-
-			// check there's somewhere to go, and if we're already there.
-			if (presentation && presentations.currentPresentation != presentationId) {
-
-				presentations.currentPresentation = presentationId;
-				$('#main').html('');
-				$('.note-current').removeClass('note-current')
-				$('#' + presentationId + "-note").addClass('note-current');
-
-
-				$('#presentationTmpl').tmpl(presentation).appendTo('#main');
-
-				$('.horizontal-carousel').imagesLoaded(presenter.revealSlideshow);
-
-				$('#main .slidewrap').carousel({
-					slider: '.horizontal-carousel-slider',
-					slide: '.horizontal-carousel-slide',
-					addPagination: true,
-					addNav: true,
-					callback: that.pushSlideNo,
-					speed: 300 // ms.
-				});
-				if (slideNo) {
-					$('.carousel-tabs li').eq(slideNo).find('a').click();
-				}
-
-			// assume we've just changed a slide number
-			} else if (presentation) {
-
-				$('.carousel-tabs li').eq(slideNo).find('a').click();
-
-			// md5('contents') === "98bf7d8c15784f0a3d63204441e1e2aa"
-			// I know this is inellegant, but it's a temporary solution
-			}else if (presentations['98bf7d8c15784f0a3d63204441e1e2aa']) {
-				$('header').removeClass('minimized');
-				for (i = 0, l = presentations['98bf7d8c15784f0a3d63204441e1e2aa'].notes.length; i < l; i += 1) {
-					liveContent += (presentations['98bf7d8c15784f0a3d63204441e1e2aa'].notes[i]).note;
-				}
-				$presentationNote = $('<div />', {
-						'class': "note note-current",
-						html: liveContent
-						});
-				$('body').addClass('header-visible').append($presentationNote);
-
-		}
-
 		});
 
 		// Since the event is only triggered when the hash changes, we need
@@ -100,9 +44,56 @@ var presenter = {
 		$(window).trigger( "hashchange" );
 	},
 
+	newMenuStatus : function () {
+		$('header').toggleClass('minimized');
+		$('body').toggleClass('header-visible');
+		if ($('body').hasClass('header-visible')) {
+			$('.note-minimized').removeClass('note-minimized');
+		} else {
+			$('.note').addClass('note-minimized');
+		}
+	},
+
+	// reveal a slideshow
 	revealSlideshow : function () {
 		$('.throbber').remove();
 		$('.horizontal-carousel').removeClass('horizontal-carousel-hidden');
+	},
+
+	// load some new content
+	newContent : function (opts) {
+		var that = this,
+			status = that.status;
+
+		$('#main').html('');
+		$('.note-current').removeClass('note-current')
+		$('#' + opts.presentationId + "-note").addClass('note-current');
+
+		$('#presentationTmpl').tmpl(opts.presentation).appendTo('#main');
+
+		$('.horizontal-carousel').imagesLoaded(presenter.revealSlideshow);
+
+		$('#main .slidewrap').carousel({
+			slider: '.horizontal-carousel-slider',
+			slide: '.horizontal-carousel-slide',
+			addPagination: true,
+			addNav: true,
+			callback: presenter.pushSlideNo,
+			speed: 300 // ms.
+		});
+
+		if (opts.slideNo) {
+			this.newSlide(opts.slideNo);
+		}
+
+		if (opts.menu == "true" && opts.menu != status.m) {
+			that.newMenuStatus();
+		}
+	},
+
+	// change slide position
+	newSlide : function (slideNo) {
+		$('.carousel-tabs li').eq(slideNo).find('a').click();
 	},
 
 	// build the nav
@@ -138,7 +129,6 @@ var presenter = {
 						'id': key,
 						text: title});
 
-
 					$('body')
 						.append($presentationNote);
 				} else {
@@ -159,12 +149,81 @@ var presenter = {
 	},
 
 	pushSlideNo : function () {
-		$.bbq.pushState({ s: $('.carousel-active-slide').index() });
+		var state = $.bbq.getState()
+			index = $('.carousel-active-slide').index();
+		// set the cached state so the hash change event 
+		// doesn't fire based on this change
+
+		presenter.status.s = index;
+		$.bbq.pushState({ s: index });
 	}
 
 };
 
+// Bind a callback that executes when document.location.hash changes.
+$(window).bind( "hashchange", function(e) {
 
+//	console.log('hash change');
+
+	var hash = $.bbq.getState(),
+		presentationId = hash.p,
+		slideNo = hash.s,
+		menu = hash.m,
+		presentation = presentations[presentationId],
+		status = presenter.status,
+		present = presenter,
+		liveContent = "";
+
+	$(".main-nav a").removeClass('nav-item-link-active');
+	$('#' + presentationId).addClass('nav-item-link-active');
+
+	// now let's find out what changed
+
+	// No presentation? We're on the front page
+	if (!presentation) {
+		$('header').removeClass('minimized');
+		try {
+			// md5('contents') === "98bf7d8c15784f0a3d63204441e1e2aa"
+			// I know this is inellegant, but it's a temporary solution
+			
+			for (i = 0, l = presentations['98bf7d8c15784f0a3d63204441e1e2aa'].notes.length; i < l; i += 1) {
+				liveContent += (presentations['98bf7d8c15784f0a3d63204441e1e2aa'].notes[i]).note;
+			}
+			$presentationNote = $('<div />', {
+					'class': "note note-current",
+					html: liveContent
+					});
+			$('body').addClass('header-visible').append($presentationNote);
+		} catch (err) {
+			console.log(err);
+		}
+
+	// no status? Then it's first page load via a link, run everything.
+	// check there's somewhere to go, and if we're already there.
+	} else if (!status || presentation && presentationId != status.p){
+		present.newContent({
+			presentation : presentation, 
+			presentationId : presentationId, 
+			slideNo : slideNo,
+			menu : menu
+			});
+
+	// User has changed slide number
+	// most likely by using the back button.
+	} else if (slideNo != status.s) {
+		present.newSlide(slideNo);
+
+	// toggle the menu
+	} else if (menu != status.m) {
+		present.newMenuStatus();
+
+	// error handling
+	} else {
+//		console.log('saw nothing', hash, status);
+	}
+
+	presenter.status = hash;
+});
 
 $(document).ready(function() { 
 	presenter.init();
