d92742387ff2d3f5063dda441a274dad3312ecfd
Nicholas Evans <nick@nick-evans.com>
2011-09-01 10:54:31 -0400

    Tidying

--- index.php
+++ index.php
@@ -74,10 +74,6 @@ include_once "_/includes/markdown.php";
   <link rel="stylesheet" href="_/css/style.css">
   
   <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
-
-  <!-- All JavaScript at the bottom, except this Modernizr build incl. Respond.js
-       Respond is a polyfill for min/max-width media queries. Modernizr enables HTML5 elements & feature detects; 
-       for optimal performance, create your own custom Modernizr build: www.modernizr.com/download/ -->
   <script src="_/js/libs/modernizr-2.0.6.min.js"></script>
 </head>
 
